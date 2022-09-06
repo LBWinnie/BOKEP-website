32b8c8aca8b8951bf242ee81a437bce4e987190c
LBWinnie <206589036@qq.com>
2022-09-06 22:17:20 +0800

    deploy

---
+++ precache-manifest.4d048e8da16e0fe989bffc59ea8b78ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376df53a7df795c98c8a",
+    "url": "/BOKEP-website/css/app.c6055b1b.css"
+  },
+  {
+    "revision": "78987b0c384182b14c5106b4fce6fbf3",
+    "url": "/BOKEP-website/index.html"
+  },
+  {
+    "revision": "376df53a7df795c98c8a",
+    "url": "/BOKEP-website/js/app.58430a3f.js"
+  },
+  {
+    "revision": "11911204fc12d85c86d9",
+    "url": "/BOKEP-website/js/chunk-vendors.0dc91db2.js"
+  },
+  {
+    "revision": "3978de6544fe227f954bc8b4b59be914",
+    "url": "/BOKEP-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BOKEP-website/robots.txt"
+  }
+]);
